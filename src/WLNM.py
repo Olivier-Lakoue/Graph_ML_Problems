0c8eacb1c7932cb8bf339e1689b153cc1c3ebb80
mgirardot <m.michael.girardot@gmail.com>
2018-07-09 05:24:15 +0200

    WLNM

--- src/WLNM.py
+++ src/WLNM.py
@@ -40,9 +40,9 @@ class PaletteWL():
             sg = nx.Graph(nx.subgraph(self.graph, Vk))
         return sg
 
-    def subgraph_colors(self, edge):
+    def subgraph_colors(self, g, edge):
         """
-        Get the subgraph of nodes around the provided initial edge.
+        Get the colors of nodes around the provided initial edge.
         :param edge: tuple(u,v)
         :return: networkx graph
         """
@@ -56,23 +56,81 @@ class PaletteWL():
         ngb_idx = 0
         # get the neighbors
         while len(Vk) < self.k:
-            nbg = list(nx.neighbors(self.graph, Vk[ngb_idx]))
+            nbg = list(nx.neighbors(g, Vk[ngb_idx]))
             # store the distance of the node from the center as the
             # initial color of the node
-            colors.extend([self.dist(i, edge, self.graph) for i in nbg])
+            colors.extend([self.dist(i, edge) for i in nbg])
+            # store the nodes
+            Vk.extend(nbg)
+            # next index
+        # crop the lists
+        return Vk[:self.k], colors[:self.k]
 
+    def dist(self, v, e):
+        """
+        Compute the distance with the shortest path length from the edge
+        :return:
+        """
+        sp_x = nx.shortest_path_length(self.graph, v, e[0])
+        sp_y = nx.shortest_path_length((self.graph, v, e[1]))
+        return np.sqrt(sp_x * sp_y)
 
-    def dist(self):
-        pass
+    def hashWL(self, x, Vk, colors):
+        """
+        Weisfeiler & Lehman hashing
+        :return:
+        """
+        position = Vk.index(x)
+        # Sum Vk
+        s_Vk = np.ceil(np.sum([np.log(primesieve.nth_prime(i)) for i in colors]))
+        # sum neighbors colors
+        nbg_x = list(nx.neighbors(self.graph, Vk[position]))
+        s_nbg_x = np.sum([np.log(primesieve.nth_prime(i)) for i in
+                          [colors[Vk.index(y)]]
+                          for y in nbg_x if y in Vk])
+        return colors[position] + s_nbg_x / s_Vk
 
-    def hashWL(self):
-        pass
+    def real_to_colors(self, colors):
+        """
+        Reduce hash real numbers to sequence of integer
+        :param colors:
+        :return:
+        """
+        new_colors = colors.copy()
+        # number of colors
+        n_cols = len(np.unique(new_colors))
+        color_list = []
+        for i in range(1, n_cols+1):
+            # minimum real value from the initial list
+            min_col = np.min(new_colors)
+            # consume the hash value
+            new_colors.pop(new_colors.index(min_col))
+            color_list.append(i)
+            # if multiple items have the same hash value
+            # add as many item of the same color to the list
+            while min_col in new_colors:
+                min_col = new_colors.pop(new_colors.index(min_col))
+                color_list.append(i)
+        return color_list
 
-    def real_to_colors(self):
-        pass
-
-    def WLgraphLab(self):
-        pass
+    def WLgraphLab(self, e):
+        """
+        Get the subgraph and it's palette-WL colors for ordering
+        :return:
+        """
+        g = self.sub_graph(e)
+        Vk, colors = self.subgraph_colors(g, e)
+        colors = self.real_to_colors(colors)
+        i = 0
+        while True:
+            new_colors = [self.hashWL(v, Vk, colors) for v in Vk]
+            new_colors = self.real_to_colors(new_colors)
+            i += 1
+            if (new_colors == colors) or i>100:
+                break
+            else:
+                colors = new_colors
+        return Vk, colors
 
     def adj_mat_subgraph(self):
         pass
